7cdd365f0474349a4469664db6cb2c04a6a22d7d
arclx724 <arclx724@gmail.com>
2026-01-15 19:38:46 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# config.py
+import os
+
+API_ID = 12345  # Apna API ID dalein
+API_HASH = "your_api_hash"
+BOT_TOKEN = "your_bot_token"
+
+# Database & AI
+MONGO_URI = "mongodb+srv://arclx724_db_user:arclx724_db_user@cluster0.czhpczm.mongodb.net/?appName=Cluster0"
+OPENROUTER_API_KEY = "sk-or-v1-..." 
+
+# Sudo users (Admins)
+SUDOERS = [12345678, 87654321]
+PREFIXES = ["/", "!", "."]
